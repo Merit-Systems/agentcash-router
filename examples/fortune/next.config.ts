3442701088cce752d9ce8b898fcda7a81f91b0d9
Mason Hall <masonhall@gmail.com>
2026-07-07 15:56:38 -0400

    eslint

--- examples/fortune/next.config.ts
+++ examples/fortune/next.config.ts
@@ -2,9 +2,6 @@ import path from 'node:path';
 import type { NextConfig } from 'next';
 
 const config: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
